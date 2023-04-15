2c8933ddd1f642584787e442581e60ad349c9071
Drikus Roor <drikusroor@gmail.com>
2023-04-15 22:58:36 +0200

    chore: Rename test that had the same name as another test

--- tests/unit/test_json_parser.py
+++ tests/unit/test_json_parser.py
@@ -67,7 +67,7 @@ class TestParseJson(unittest.TestCase):
             fix_and_parse_json(json_str, try_to_fix_with_gpt=False), good_obj
         )
 
-    def test_invalid_json_leading_sentence_with_gpt(self):
+    def test_invalid_json_leading_sentence_with_gpt_alt(self):
         # Test that a REALLY invalid JSON string raises an error when try_to_fix_with_gpt is False
         json_str = """I will first need to browse the repository (https://github.com/Torantulino/Auto-GPT) and identify any potential bugs that need fixing. I will use the "browse_website" command for this.
 
